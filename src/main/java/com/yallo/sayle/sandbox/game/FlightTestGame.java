408d46cf01426e18a8283c71ae77ee8f3b5d594a
Thinner Thinker <thinnrthinkr@gmail.com>
2024-04-08 15:16:52 +0300

    add bigger obstacle course

--- src/main/java/com/yallo/sayle/sandbox/game/FlightTestGame.java
+++ src/main/java/com/yallo/sayle/sandbox/game/FlightTestGame.java
@@ -7,6 +7,7 @@ import com.yallo.sayle.sandbox.input.Input;
 import org.joml.Vector3f;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import static org.lwjgl.glfw.GLFW.*;
 
@@ -22,22 +23,45 @@ public class FlightTestGame extends Game {
 
     @Override
     public void initialize() {
-        float fovX = 25f * (float)Math.PI / 180f, fovY = 25f * (float)Math.PI / 180f;
-        int sampleSize = 20;
+        float fovX = 45f * (float)Math.PI / 180f, fovY = 45f * (float)Math.PI / 180f;
+        int sampleSize = 50;
 
-        camera = new Camera(0.25f, 0.5f, 10f);
+        camera = new Camera(0.25f, 0.15f, 10f);
         character = new Character(new CharacterState(new Vector3f(0f, 0f, 0f),
                 10f),
-                0.15f);
+                0.5f);
 
-        ArrayList<SolidBox> obstacles = new ArrayList<>();
-        obstacles.add(new SolidBox(new Vector3f(-10f, -20f, -52f), new Vector3f(2f, 5f, -50f)));
-        obstacles.add(new SolidBox(new Vector3f(-20f, -20f, -72f), new Vector3f(10f, 20f, -70f)));
+        ArrayList<SolidBox> obstacles = createHoles();
 
         course = new ObstacleCourse(obstacles);
 
         flight = new FlightBehavior(sampleSize,20f, fovX, fovY);
         costFunction = RegionEvaluatorFunction.safest(fovX, fovY, sampleSize);
+    }
+
+    public ArrayList<SolidBox> createHoles() {
+        ArrayList<SolidBox> obstacles = new ArrayList<>();
+        Random random = new Random();
+
+        float holeSize = 3, holeDeviation = 6;
+        float borderSize = 50; // The half-size of each SolidBox
+
+        /* obstacles.add(new SolidBox(new Vector3f(-borderSize - 1, -borderSize - 1, -30), new Vector3f(-borderSize, borderSize, -100)));
+        obstacles.add(new SolidBox(new Vector3f(borderSize, -borderSize - 1, -30), new Vector3f(borderSize + 1, borderSize, -100)));
+        obstacles.add(new SolidBox(new Vector3f(-borderSize - 1, -borderSize - 1, -30), new Vector3f(borderSize, -borderSize, -100)));
+        obstacles.add(new SolidBox(new Vector3f(-borderSize, borderSize, -30), new Vector3f(borderSize, borderSize + 1, -100))); */
+
+        for (float z = -30; z >= -300; z -= 30) {
+            float centerX = random.nextFloat() * holeDeviation - holeDeviation / 2;
+            float centerY = random.nextFloat() * holeDeviation - holeDeviation / 2;
+
+            obstacles.add(new SolidBox(new Vector3f(-borderSize, -borderSize, z), new Vector3f(centerX, borderSize, z + 1)));
+            obstacles.add(new SolidBox(new Vector3f(centerX + holeSize, -borderSize, z), new Vector3f(borderSize, borderSize, z + 1)));
+            obstacles.add(new SolidBox(new Vector3f(-borderSize, -borderSize, z), new Vector3f(borderSize, centerY, z + 1)));
+            obstacles.add(new SolidBox(new Vector3f(-borderSize, centerY + holeSize, z), new Vector3f(borderSize, borderSize, z + 1)));
+        }
+
+        return obstacles;
     }
 
     boolean isMouseInWindow() {
@@ -47,8 +71,9 @@ public class FlightTestGame extends Game {
 
     @Override
     public void update(double dt) {
-        if (!Input.isKeyDown(GLFW_KEY_SPACE)) {
-            return;
+        if (Input.isKeyDown(GLFW_KEY_SPACE)) {
+            character.pushInput(flight.desiredInput(character.state.clone(), course, costFunction), (float) dt);
+            character.state.position.add(new Vector3f(character.state.forward).mul(character.state.velocity * (float) dt));
             /*mouseCaptured = !mouseCaptured;
 
             if (mouseCaptured) {
@@ -58,9 +83,7 @@ public class FlightTestGame extends Game {
             }*/
         }
 
-        //flight.desiredInput(character.state.clone(), course, costFunction);
-        character.pushInput(flight.desiredInput(character.state.clone(), course, costFunction), (float) dt);
-        character.state.position.add(new Vector3f(character.state.forward).mul(character.state.velocity * (float) dt));
+
         character.updateTransform();
 
         camera.setPivotPosition(character.state.position);
